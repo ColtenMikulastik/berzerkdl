b7c6e2e8c0ff9a5fb67eb05c902018dedc84c3e2
Colten <mikulastikc@gmail.com>
2022-08-24 16:45:42 -0500

    added comments and an exception block

--- __main__.py
+++ __main__.py
@@ -2,27 +2,42 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import time
 
 def main():
+    # this is the url for the page I want to take
     url = "https://readberserk.com/chapter/berserk-chapter-a0/"
-
+    
+    # grab the html
     r = requests.get(url)
 
+    #parse the html
     soup = BeautifulSoup(r.text, "html.parser")
-
+    
+    # find all images
     images = soup.find_all('img')
     
-    print(images)
+    # loop through all the images
     i = 1
+    os.chdir("ep1")
     for image in images:
         name = "image_" + str(i)
         i += 1
+        # get the image section
         link = image["src"]
         print(name)
         print(link)
-        with open(name + ".jpg", "wb") as f:
+        try:
+            # ask nicely for the jpg
             im = requests.get(link)
+            # if rude try again
+        except OSError:
+            print("failed on " + name)
+            time.sleep(3)
+            im = requests.get(link)
+
+        # write it to a file
+        with open(name + ".jpg", "wb") as f:
             f.write(im.content)
 
 
