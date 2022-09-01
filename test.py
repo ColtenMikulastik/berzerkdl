4bb0bf09183887c00525197e60ab3f0a957e3b22
Colten <mikulastikc@gmail.com>
2022-09-01 16:43:19 -0500

    proof of concept qing and threading

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 from queue import Queue
+import time
+import threading
 
 def main():
     q = Queue()
     l_o_n = [i for i in range(1,11)]
     fill_q( q, l_o_n)
-    print_q(q)
-    print("---------------")
-    pop_list(l_o_n)
-    print("---------------")
 
-    print(l_o_n)
-    
+    # create threads
+    threads = []
+        
+    for i in range(0,10):
+        t = threading.Thread(target=download_q, args=[q])
+        threads.append(t)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 def pop_list(l_o_n):
     print(l_o_n.pop())
@@ -20,10 +28,13 @@ def fill_q( q, l_o_n):
     for i in l_o_n:
         q.put(i)
 
-def print_q(q):
+def download_q(q):
     while not(q.empty()):
-        print(q.get())
-    
+        item = q.get()
+        print("downloading: " + str(item))
+        time.sleep(int(item))
+        print("downloaded: " + str(item))
 
+        
 
 main()
